d3c0866774439c769bb420c40dc98dc62180ea64
Vanbandon <theo.vanbandon@gmail.com>
2023-12-25 23:39:31 +0100

    Correction de l'erreur lors de l'ouverture

--- assets/js/init.js
+++ assets/js/init.js
@@ -348,15 +348,15 @@ jQuery(window).load(function(){
       theme:"minimal"
     });
 
-     // PRELOADING SCREEN
-    jQuery('a:not([target="_blank"]):not([href*=#]):not([href^=mailto]):not(.fancybox-media):not(.btn.responsive-menu):not(a[href$="jpg"]):not([href$="jpeg"]):not(a[href$="gif"]):not(a[href$="png"]):not(a.ajax-link):not(.menu-btn)').on('click', function(){
-      var href = jQuery(this).attr('href');
-      jQuery('.preloader').fadeIn(300);
-      setTimeout(function(){
-        window.location = href;
-      }, 300);
-      return false;
-    });
+    // PRELOADING SCREEN
+    //jQuery('a:not([target="_blank"]):not([href*=#]):not([href^=mailto]):not(.fancybox-media):not(.btn.responsive-menu):not(a[href$="jpg"]):not([href$="jpeg"]):not(a[href$="gif"]):not(a[href$="png"]):not(a.ajax-link):not(.menu-btn)').on('click', function(){
+    //  var href = jQuery(this).attr('href');
+    //  jQuery('.preloader').fadeIn(300);
+    //  setTimeout(function(){
+    //    window.location = href;
+    //  }, 300);
+    //  return false;
+    //});
 });
 
 //===================
